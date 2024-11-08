50c7a21932347493642db29aadd465ce5872bd27
lede-dev <lede.7716@gmail.com>
2024-11-08 23:51:04 +0900

    feat: add player classes

---
+++ Source/ProjectDelta/Player/GameplayPlayerCharacter.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright Lede Studios. All Rights Reserved.
+
+
+#include "GameplayPlayerCharacter.h"
+
+#include "Camera/CameraComponent.h"
+
+AGameplayPlayerCharacter::AGameplayPlayerCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>("Camera");
+	CameraComponent->SetupAttachment(GetRootComponent());
+}
+
+void AGameplayPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/ProjectDelta/Player/GameplayPlayerCharacter.h
@@ -0,0 +1,27 @@
+﻿// Copyright Lede Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "GameplayPlayerCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class PROJECTDELTA_API AGameplayPlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AGameplayPlayerCharacter();
+
+public:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Player")
+	TObjectPtr<UCameraComponent> CameraComponent;
+	
+};
